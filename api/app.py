5c88bf68018f9a3143a069c1366a201983eaec2d
azcao95 <azcao95@gmail.com>
2026-04-03 18:15:38 -0500

    specified host and port

--- api/app.py
+++ api/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request, jsonify
 from api.process_text import process_text_si, process_text_tc
 
@@ -18,7 +20,8 @@ def process():
     return jsonify(result), 200
 
 def main():
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
 
 if __name__ == "__main__":
     main()
